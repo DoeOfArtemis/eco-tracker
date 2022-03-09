2bf060f973e35b672cf0960cddcded2c32d82f1b
NeringaP <neringa.pet@gmail.com>
2022-03-09 10:15:08 +0200

    Merged databaseFile branch

--- app/src/main/java/com/example/ecotracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ecotracker/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.ecotracker.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
+import com.example.ecotracker.MainActivity;
 import com.example.ecotracker.R;
+import com.example.ecotracker.dao.UserDao;
+import com.example.ecotracker.database.EcoTrackerDatabase;
+import com.example.ecotracker.ui.neringosui.FirstNeringaActivity;
+import com.example.ecotracker.ui.neringosui.SecondNeringaActivity;
 
 public class HomeFragment extends Fragment {
+
+    EcoTrackerDatabase db;
+    TextView textView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,7 +33,16 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        db = EcoTrackerDatabase.getDatabase(this.getContext());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        textView = (TextView) view.findViewById(R.id.courseTitle);
+        textView.setText(db.courseDao().findCourseById(1).getName() + " course");
+
+        return view;
+
+
     }
 }
