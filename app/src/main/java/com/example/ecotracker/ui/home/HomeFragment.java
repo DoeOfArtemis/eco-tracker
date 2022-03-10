e9f1323319ca643616fc690b9ddab3cc9ffc47c6
NeringaP <neringa.pet@gmail.com>
2022-03-10 16:28:04 +0200

    Updated HomeFragment

--- app/src/main/java/com/example/ecotracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ecotracker/ui/home/HomeFragment.java
@@ -1,8 +1,5 @@
 package com.example.ecotracker.ui.home;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,104 +7,93 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.ecotracker.MainActivity;
 import com.example.ecotracker.R;
-import com.example.ecotracker.dao.UserDao;
 import com.example.ecotracker.database.EcoTrackerDatabase;
 import com.example.ecotracker.model.Task;
-import com.example.ecotracker.ui.neringosui.FirstNeringaActivity;
-import com.example.ecotracker.ui.neringosui.SecondNeringaActivity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    static EcoTrackerDatabase db;
-    TextView textView;
-    static ArrayList<Task> tasksNotCompleted;
-    List<Task> allTasksInCourse;
     static ListView listView;
     static CustomAdapter adapter;
+    static EcoTrackerDatabase db;
+    static ArrayList<Task> tasks;
     static ProgressBar progressBar;
     static int totalPoints;
     int courseId = 1;
-
-    Context context = this.getContext();
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        db = EcoTrackerDatabase.getDatabase(this.getContext());
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        db = EcoTrackerDatabase.getDatabase(this.getContext());
 
-        textView = (TextView) view.findViewById(R.id.courseTitle);
-        textView.setText(db.courseDao().findCourseById(courseId).getName() + " course");
+        setCourseTitle(view);
+        getTasksListFromDB();
+        int totalPointsOfCourse = getTotalPointsOfCourse();
+        setProgressBarMax(view, totalPointsOfCourse);
 
-
-        tasksNotCompleted = (ArrayList) db.taskDao().getAllNotCompletedByCourseId(courseId);
-
-        int totalPointsOfCourse = 0;
-        allTasksInCourse = db.taskDao().getAllByCourseId(courseId);
-        for (Task task : allTasksInCourse) {
-            totalPointsOfCourse += task.getPoints();
-        }
-
-        progressBar = (ProgressBar) view.findViewById(R.id.progressBarOfCourse);
-        progressBar.setMax(totalPointsOfCourse);
-        int progress = 0;
-
-        adapter = new CustomAdapter(tasksNotCompleted, this.getContext());
+        adapter = new CustomAdapter(tasks, this.getContext());
         listView = (ListView) view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
+        /*
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-               Task task = (Task) tasksNotCompleted.get(position);
-               task.setCompleted(!task.isCompleted());
-
-                String clickedItem = (String) listView.getItemAtPosition(position);
-                Toast.makeText(context, clickedItem, Toast.LENGTH_SHORT).show();
-
-                db.taskDao().findTaskById(task.getId()).setCompleted(true);
-                db.taskDao().update(task);
-
-                adapter.notifyDataSetChanged();
-
 
             }
         });
+         */
         return view;
     }
 
+    private void setProgressBarMax(View view, int totalPointsOfCourse) {
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBarOfCourse);
+        progressBar.setMax(totalPointsOfCourse);
+    }
+
+    private int getTotalPointsOfCourse() {
+        int totalPointsOfCourse = 0;
+        tasks = (ArrayList<Task>) db.taskDao().getAllByCourseId(courseId);
+        for (Task task : tasks) {
+            totalPointsOfCourse += task.getPoints();
+        }
+        return totalPointsOfCourse;
+    }
+
+    private void getTasksListFromDB() {
+        tasks = (ArrayList) db.taskDao().getAllByCourseId(courseId);
+    }
+
+    private void setCourseTitle(View view) {
+        TextView textView = (TextView) view.findViewById(R.id.courseTitle);
+        textView.setText(db.courseDao().findCourseById(courseId).getName() + " course");
+    }
+
     public static void removeItem(int position) {
-        tasksNotCompleted.remove(position);
+        tasks.remove(position);
     }
     public static void updateItem(int position) {
-        Task task = tasksNotCompleted.get(position);
+        Task task = tasks.get(position);
         task.setCompleted(true);
         db.taskDao().update(task);
     }
 
     public static void getPoints(int position) {
-        int points = tasksNotCompleted.get(position).getPoints();
+        int points = tasks.get(position).getPoints();
         totalPoints += points;
         progressBar.setProgress(totalPoints);
         listView.setAdapter(adapter);
