0cf84ebc951d082db958f23bb6f85d773a1cfec2
NeringaP <neringa.pet@gmail.com>
2022-03-10 16:44:03 +0200

    Registration without email

--- app/src/main/java/com/example/ecotracker/RegisterActivity.java
+++ app/src/main/java/com/example/ecotracker/RegisterActivity.java
@@ -6,19 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.ecotracker.database.EcoTrackerDatabase;
+import com.example.ecotracker.model.User;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText name;
+    EditText password;
+    EcoTrackerDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        db = EcoTrackerDatabase.getDatabase(getApplicationContext());
+        username = findViewById(R.id.input_new_username);
+        name = findViewById(R.id.input_new_name);
+        password = findViewById(R.id.input_new_password);
         Button registerNewUser = (Button) findViewById(R.id.add_new_user);
-
         registerNewUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                User user = new User();
+                user.setUserName(username.getText().toString());
+                user.setName(name.getText().toString());
+                user.setPassword(password.getText().toString());
+                db.userDao().insert(user);
+
                 startActivity(new Intent(RegisterActivity.this,MainActivity.class));
             }
         });
