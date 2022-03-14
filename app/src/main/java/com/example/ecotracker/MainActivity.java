f9091b1c0d49f1c46a1d6146d754bcd3a685be8c
Roberta Zvejniece <zvejniece.roberta@gmail.com>
2022-03-14 18:51:43 +0200

    Added course information

--- app/src/main/java/com/example/ecotracker/MainActivity.java
+++ app/src/main/java/com/example/ecotracker/MainActivity.java
@@ -2,21 +2,22 @@ package com.example.ecotracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.example.ecotracker.database.EcoTrackerDatabase;
 import com.example.ecotracker.databinding.ActivityMainBinding;
 import com.example.ecotracker.ui.courses.CoursesFragment;
+import com.example.ecotracker.ui.courses.FirstCourseFragment;
+import com.example.ecotracker.ui.courses.SecondCourseFragment;
 import com.example.ecotracker.ui.home.HomeFragment;
 import com.example.ecotracker.ui.profile.ProfileFragment;
 
@@ -25,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     EcoTrackerDatabase db;
 
-
-    Button firstCourseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,17 +93,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-        // Start of NAVIGATION part after user successfully logged in
-
     }
 
-    //method to replace current fragment
+    //method to replace current fragment from navigation bar
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    //methods to click on a course to view it
+    public void viewCourse(View view) {
+        switch (view.getId()) {
+            case R.id.how_to_recycle:
+                replaceFragment(new FirstCourseFragment());
+                break;
+            case R.id.zero_waste_basics:
+                replaceFragment(new SecondCourseFragment());
+                break;
+        }
     }
 
 }
